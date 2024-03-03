6bb2db7655c904100130ac5b5e2885ff20577804
Praveena Lingala <reachveena03@gmail.com>
2024-03-03 01:11:39 -0500

    updated read me

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_Visa",
+    version="0.0.0",
+    author="Praveena",
+    author_email="reachveena03@gmail.com",
+    packages=find_packages(),
+    )
