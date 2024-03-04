ff29fb39f2f9582df5d1be6114778806a5f7fbcc
Praveena Lingala <reachveena03@gmail.com>
2024-03-04 00:20:49 -0500

    basic done except EDA

--- demo.py
+++ demo.py
@@ -1,4 +1,8 @@
 from us_Visa.logger import logging
-from us_Visa.logger import USvisaException
+from us_Visa.exception import USvisaException
 import sys
+try:
+    a=1/"10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
 
