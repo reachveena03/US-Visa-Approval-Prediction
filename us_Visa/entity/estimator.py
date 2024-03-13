c9ebf7dab1c39125eacd2eefb71d5aae394a4f32
Praveena Lingala <reachveena03@gmail.com>
2024-03-12 23:42:20 -0400

    Transformation

---
+++ us_Visa/entity/estimator.py
@@ -0,0 +1,17 @@
+import sys
+
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from us_Visa.exception import USvisaException
+from us_Visa.logger import logging
+
+class TargetValueMapping:
+    def __init__(self):
+        self.Certified:int = 0
+        self.Denied:int = 1
+    def _asdict(self):
+        return self.__dict__
+    def reverse_mapping(self):
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
